6f8bbd920a84374dbefc06ea32d4cdb9afe6da93
Junior Ubarana <elci.junior@gmail.com>
2021-01-20 18:23:54 -0300

    Link test

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 function Home() {
   return <div>
     <h1>Hello, world</h1>
     <p>My first test with NextJS</p>
-    <a href="/sobre">Acess About page</a>
+    <Link href='/sobre'>
+      <a>Acess About page</a>
+    </Link>
   </div>
-}
+};
 
-export default Home
+export default Home;
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,9 +1,13 @@
+import Link from 'next/link';
+
 function Sobre() {
   return <div>
     <h1>Hello, world</h1>
     <p>My first test with NextJS</p>
-    <a href="/">Acess Home page</a>
+    <Link href='/'>
+      <a>Acess Home page</a>
+    </Link>
   </div>
-}
+};
 
-export default Sobre
+export default Sobre;
