b0539d46051960fe8621d76288b2d810ac8f4a69
Junior Ubarana <elci.junior@gmail.com>
2021-01-20 18:18:38 -0300

    About page created

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,9 +1,9 @@
-function Home() {
+function Sobre() {
   return <div>
     <h1>Hello, world</h1>
     <p>My first test with NextJS</p>
-    <a href="/sobre">Acess About page</a>
+    <a href="/">Acess Home page</a>
   </div>
 }
 
-export default Home
+export default Sobre
